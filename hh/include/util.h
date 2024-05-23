9d7be5c0aebd5fdd88a86d5427bf45d8a334ef89
jk-zjz <3514805258@qq.com>
2024-05-23 19:42:05 +0800

    协程模块准备

--- hh/include/util.h
+++ hh/include/util.h
@@ -8,11 +8,16 @@
 #include <unistd.h>
 #include <cstdint>
 #include <thread>
+#include <vector>
+#include <string>
 namespace hh {
     //线程
     pid_t GetThreadID();
 
     //携程
     uint32_t GetFiberID();
+
+    void Backtrace(std::vector<std::string>& bt, int size, int skip);
+    std::string BacktraceToString(int size, int skip=2, const std::string& prefix = "");
 }
 #endif //HH_UTIL_H
--- hh/util.cc
+++ hh/util.cc
@@ -3,6 +3,8 @@
 //
 #include "util.h"
 #include <sys/syscall.h>
+#include <sstream>
+#include <execinfo.h>
 
 
 namespace hh {
@@ -18,4 +20,43 @@ namespace hh {
         return 0;
     }
 
+    /**
+     * 获取程序的回溯信息。
+     *
+     * 该函数通过调用系统库backtrace和backtrace_symbols，获取指定数量的调用栈信息，并将其转换为可读的字符串形式，
+     * 然后将这些信息添加到提供的字符串向量中。
+     *
+     * @param bt 引用传递，用于存储回溯信息的字符串向量。
+     * @param size 预期收集的回溯信息条目数量。
+     * @param skip 跳过的回溯信息条目数量，通常用于跳过当前函数等无用信息。
+     */
+    void Backtrace(std::vector<std::string>& bt, int size, int skip){
+        // 分配内存以存储回溯数据。
+        void **data = (void**)malloc(sizeof(void*) * size);;
+        // 获取回溯信息。
+        size_t s= ::backtrace(data, size);
+        // 将二进制回溯数据转换为可读字符串。
+        char **strings = backtrace_symbols(data, s);
+
+        // 将转换后的回溯信息添加到向量中，跳过前skip个条目。
+        for(size_t i = skip; i < s; ++i){
+            bt.emplace_back(strings[i]);
+        }
+
+        // 释放动态分配的内存。
+        free(strings);
+        free(data);
+    }
+    /*
+     * 序列化回溯信息
+     * */
+    std::string BacktraceToString(int size, int skip,const std::string& prefix){
+        std::stringstream ss;
+        std::vector<std::string> bt;
+        Backtrace(bt, size, skip);
+        for(size_t i=skip;i<bt.size();i++){
+            ss << prefix << bt[i] << std::endl;
+        }
+        return ss.str();
+    }
 }
